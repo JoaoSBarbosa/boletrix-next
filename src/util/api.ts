5f7f41ada83419e6e9f4e4dde51864b4de4ecd05
João S. Barbosa <contato.jsbarbosa@gmail.com>
2025-06-20 22:23:36 -0300

    :recycle: prod

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,25 +1,25 @@
 import axios from "axios";
 
 // Função para obter o URL da conexão com base na localização atual
+// export function getUrlConnection(currentLocation: string) {
+//     let url = "http://localhost";
+//     let porta: string = "8080";
+//
+//     if (currentLocation.includes('45.231.133.225')) url = "http://45.231.133.225";
+//     if (currentLocation.includes('191.252.221.124')) url = "http://191.252.221.124";
+//
+//     return url + ":" + porta;
+// }
 export function getUrlConnection(currentLocation: string) {
-    let url = "http://localhost";
-    let porta: string = "8080";
+    const railwayUrl = "https://zucchini-essence-production.up.railway.app";
 
-    if (currentLocation.includes('45.231.133.225')) url = "http://45.231.133.225";
-    if (currentLocation.includes('191.252.221.124')) url = "http://191.252.221.124";
+    // Se estiver em localhost (desenvolvimento)
+    if (typeof window !== "undefined" && window.location.hostname === "localhost") {
+        return "http://localhost:8080";
+    }
 
-    return url + ":" + porta;
-}
-
-// Função para obter o URL da conexão NFC com base na localização atual
-export function getUrlConnectionNfc(currentLocation: string) {
-    let url = "http://localhost";
-    let porta: string = "8080";
-
-    if (currentLocation.includes('45.231.133.225')) url = "http://45.231.133.225";
-    if (currentLocation.includes('191.252.221.124')) url = "http://191.252.221.124";
-
-    return url + ":" + porta;
+    // Para qualquer outro caso, retorna a URL do Railway (produção)
+    return railwayUrl;
 }
 
 // Função para criar a conexão com a API, considerando a localização e o token no localStorage
